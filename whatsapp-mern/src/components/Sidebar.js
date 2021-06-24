b7c8d6373177ee493d7b1eb0055233649dd3c62a
Maneshwar <mehramaneshwar4@gmail.com>
2021-06-24 17:28:34 +0530

    added Search in the Sidebar and added style to it

--- whatsapp-mern/src/components/Sidebar.js
+++ whatsapp-mern/src/components/Sidebar.js
@@ -24,7 +24,12 @@ const Sidebar = () => {
                     </IconButton>
                 </div>
             </div>
-            <div className="sidebar__search"></div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type="text" />
+                </div>
+            </div>
             <div className="sidebar__chats"></div>
         </div>
     )
