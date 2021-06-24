1f0ca7cab715d645820c8d1697fc26eae48b8a24
Maneshwar <mehramaneshwar4@gmail.com>
2021-06-24 17:19:24 +0530

    added UI icons to the Sidebar

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-
+        <Sidebar />
       </div>
     </div>
   );
--- whatsapp-mern/src/components/Sidebar.js
+++ whatsapp-mern/src/components/Sidebar.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Sidebar.css'
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+
+
 const Sidebar = () => {
     return (
-        <div>
-            <div className="sidebar__header"></div>
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__header">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
             <div className="sidebar__search"></div>
             <div className="sidebar__chats"></div>
         </div>
